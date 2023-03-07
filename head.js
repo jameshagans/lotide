ef29388b49a2a372f7852f32833ea70b09ec5b4c
jameshagans <jamesy.h95@gmail.com>
2023-03-07 01:06:41 +0000

    Adding a function to retreive the head of an array

---
+++ head.js
@@ -0,0 +1,14 @@
+const { isUndefined } = require("lodash");
+
+const assertEqual = function (expression, expected) {
+  if (expression === expected) {
+    return console.log(`🟢🟢🟢 Assertion Passed: ${expression} === ${expected}`);
+  }
+  return console.log(`❌❌❌ Assertion Failed: ${expression} !== ${expected}`);
+};
+
+let head = function (arr) {
+  return arr[0];
+};
+console.log(assertEqual(head([5, 4, 6]), 5));
+console.log(assertEqual(head([]), undefined)); 
