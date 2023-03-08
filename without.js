875a4946e8a3aa41a0bcb77e5a7682bc6342b8e9
jameshagans <jamesy.h95@gmail.com>
2023-03-08 05:43:29 +0000

    adding without.js file that takes 2 arrays as arguments in a function and returns a new array with the elements from the second array removed from the first arrayd

---
+++ without.js
@@ -0,0 +1,46 @@
+let assertArraysEqual = function (a, b) {
+  return eqArrays(a, b);
+}
+
+
+
+function eqArrays(arr1, arr2) {
+  // check the length of arrays as if they're not the same they can't be equal 
+  if (arr1.length != arr2.length) {
+    return `❌❌❌ Assertion Failed: The arrays have a different length`;
+  } else {
+
+    // comparing each element of array 
+    for (let i = 0; i < arr1.length; i++) {
+
+      if (arr1[i] !== arr2[i]) {
+        return `❌❌❌ Assertion Failed: The arrays do not match!`;
+      }
+    }
+    return `🟢🟢🟢 Assertion Passed: The arrays are a match! `;
+  }
+}
+
+
+const without = function (source, itemsToRemove) {
+  let remainingItems = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i]))
+      remainingItems.push(source[i]);
+  }
+  return console.log(remainingItems);
+}
+
+
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"])
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+//assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+
+
+
