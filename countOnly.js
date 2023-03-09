df17155b05671bcc7e47d3ee7bf73e445afa6a82
jameshagans <jamesy.h95@gmail.com>
2023-03-09 01:30:37 +0000

    adding countOnly.js file

---
+++ countOnly.js
@@ -0,0 +1,54 @@
+const assertEqual = function (expression, expected) {
+  if (expression === expected) {
+    return console.log(`🟢🟢🟢 Assertion Passed: ${expression} === ${expected}`);
+  }
+  return console.log(`❌❌❌ Assertion Failed: ${expression} !== ${expected}`);
+};
+
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+  const results = {}
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log("result array = ", results);
+  return results;
+}
+
+
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
